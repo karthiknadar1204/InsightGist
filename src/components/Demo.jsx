d1de9a81d087b1e57fc4684f6046d5aa4c78d891
karthik <karthiknadar1204@gmail.com>
2024-01-15 14:50:09 +0530

    minor styling updates

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
@@ -6,7 +6,7 @@ import { useLazyGetSummaryQuery } from "../services/article";
 const Demo = () => {
   const [article, setArticle] = useState({
     url: "",
-    summary: "",
+    summary: "", 
   });
   const [allArticles, setAllArticles] = useState([]);
   const [copied, setCopied] = useState("");
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,26 @@
-// import React from "react";
-
-import { logo } from "../assets";
 
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
-
+        <h2 className="nav-left-header" >Insight<span className="gist" >Gist</span></h2>
         <button
           type='button'
           onClick={() =>
             window.open("https://github.com/karthiknadar1204", "_blank")
           }
           className='black_btn'
-        >
+        > 
           GitHub
         </button>
       </nav>
 
       <h1 className='head_text'>
-        Summarize Articles with <br className='max-md:hidden' />
-        <span className='orange_gradient '>OpenAI GPT-4</span>
+      Crafting Epitomes from <br className='max-md:hidden' />
+        <span className='orange_gradient '>the Vast Tapestry of Textual Chronicles</span>
       </h1>
       <h2 className='desc'>
-        Simplify your reading with Summize, an open-source article summarizer
-        that transforms lengthy articles into clear and concise summaries
+      InsightGist, the Open-Source Article Wizard, Conjures Clear and Digestible Summaries
       </h2>
     </header>
   );
